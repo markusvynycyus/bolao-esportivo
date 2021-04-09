354b0e43c32e21418bcfe6611e9453610aa952cd
marku <markusvynycyus@hotmail.com>
2021-04-09 00:07:40 -0300

    Aletraçoes Settings p Deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
