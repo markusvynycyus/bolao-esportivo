85ede3355ace07571b68bef5dfbc726596adb92b
marku <markusvynycyus@hotmail.com>
2021-04-13 00:15:20 -0300

    Criando Views para Listar jogos.

--- bets/views.py
+++ bets/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+#from cart.forms import CartAddJogoForm
+from .models import Jogo, Rodada
 
-# Create your views here.
+def jogo_list(request, rodada_slug=None):
+    rodada = None
+    rodadas = Rodada.objects.all()
+    jogos = Jogo.objects.filter(available=true)
+    if rodada_slug:
+        rodada = get_object_or_404(Rodada, slug=rodada_slug)
+        jogo = jogos.filter(rodada=rodada)
+        return
+        render(request,
+               'bets/jogo/list.html',
+               {'rodada': rodada,
+                'rodadas': rodadas,
+                'jogos': jogos})
+
+def jogo_detail(request, id, slug):
+    jogo = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    #cart_product_form = CartAddProductForm()
+    return
+    render(request,
+           'bets/jogo/detail.html',
+           {'jogo': jogo})
+           # 'cart_product_form': cart_product_form})
