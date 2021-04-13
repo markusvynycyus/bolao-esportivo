97b9f989b460cd61852d922c1fcca790fdc84e7d
marku <markusvynycyus@hotmail.com>
2021-04-13 00:21:25 -0300

    Criando a Urls.py de do app Bets

---
+++ bets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_nome ='bets'
+urlpatterns = [
+    path('', views.jogo_list, name='jogo_list'),
+    path('<slug:rodada_slug>/', views.jogo_list, name='jogo_list_by_rodada'),
+    path('<int:id>/<slug:slug>/', views.jogo_detail, name='jogo_detail'),
+]
+
